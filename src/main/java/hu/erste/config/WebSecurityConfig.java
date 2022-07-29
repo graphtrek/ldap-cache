28585206edd84ee2dec167ce06e2a26d4434ad1f
Imre <imre.tatai@gmail.com>
2022-07-29 10:42:05 +0200

    WebSecurityCustomizer In Spring Security 5.7

--- src/main/java/hu/erste/config/WebSecurityConfig.java
+++ src/main/java/hu/erste/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package hu.erste.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,4 +42,8 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+    }
 }
